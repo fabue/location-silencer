5d7c6f9f34bef87ab6372320cc0ebd8a64f1242f
Fabian Bützow <fabian@artcom.de>
2016-04-26 12:48:13 +0200

    Create LocationRequest

--- app/src/main/java/de/artcom/locationsilencer/MainActivity.java
+++ app/src/main/java/de/artcom/locationsilencer/MainActivity.java
@@ -1,20 +1,25 @@
 package de.artcom.locationsilencer;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int LOCATION_UPDATE_INTERVAL = 5000;
+    public static final int LOCATION_UPDATE_FASTEST_INTERVAL = 2000;
+
     private GoogleApiClient mGoogleApiClient;
 
     @Override
@@ -45,6 +50,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         Log.d(TAG, "getLocationUpdatesClicked");
         ((TextView) findViewById(R.id.currentLatitude)).setText("foo");
         ((TextView) findViewById(R.id.currentLongitude)).setText("bar");
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(LOCATION_UPDATE_FASTEST_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
