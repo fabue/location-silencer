aec66e909a62a9d8dfc6bbee725785b0cd09689c
Fabian Bützow <fabian@artcom.de>
2016-04-26 12:35:54 +0200

    Create mGoogleApiClient and setup callbacks

--- app/src/main/java/de/artcom/locationsilencer/MainActivity.java
+++ app/src/main/java/de/artcom/locationsilencer/MainActivity.java
@@ -1,20 +1,35 @@
 package de.artcom.locationsilencer;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     @Override
@@ -57,5 +72,20 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "getLocationUpdatesClicked");
         ((TextView) findViewById(R.id.currentLatitude)).setText("foo");
         ((TextView) findViewById(R.id.currentLongitude)).setText("bar");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
     }
 }
