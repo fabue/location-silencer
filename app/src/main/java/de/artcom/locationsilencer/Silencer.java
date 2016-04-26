7df0b20215668ab63ae0b2ee6c419f9065900d3a
Fabian Bützow <fabian@artcom.de>
2016-04-26 14:20:01 +0200

    Show Toast when Silencer starts

--- app/src/main/java/de/artcom/locationsilencer/Silencer.java
+++ app/src/main/java/de/artcom/locationsilencer/Silencer.java
@@ -2,7 +2,10 @@ package de.artcom.locationsilencer;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Silencer extends IntentService{
 
@@ -15,5 +18,12 @@ public class Silencer extends IntentService{
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d(TAG, "onHandleIntent");
+
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(Silencer.this, "Silencer started", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
